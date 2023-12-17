15de130528ba3a9092cbce1a68772797dc4bea1d
MuhammadAmmar001 <muhammadammar001002@gmail.com>
2023-12-17 19:47:34 +0500

    Test cases for Registration

--- RegistrationTest.java
+++ RegistrationTest.java
@@ -1,82 +1,99 @@
-
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class RegistrationTest {
+
     @Test
     public void testEmptyNameField() {
-        MyFrame frame = new MyFrame();
-        frame.setVisible(true);
-        frame.tname.setText("");
-        frame.term.setSelected(true);
-        frame.sub.doClick(); // Simulate button click
-        assertEquals("Please enter your name", frame.res.getText());
+        Registration frame = new Registration();
+        frame.nameTextField.setText("");
+        frame.mobileTextField.setText("12345567890");
+        frame.passwordField.setText("*passworD123");
+        frame.termsCheckBox.setSelected(true);
+        frame.submitButton.doClick();
+
+        assertEquals("Please enter your name.",
+                frame.resultTextArea.getText());
     }
 
     @Test
     public void testInvalidMobileNumber() {
-        MyFrame frame = new MyFrame();
-        frame.setVisible(true);
-        frame.tmno.setText("4a4");
-        frame.tname.setText("abc");
-        frame.tpassword.setText("abc");
-        frame.term.setSelected(true);
-        frame.sub.doClick();
-        assertEquals("Please enter a valid mobile number", frame.res.getText());
+        Registration frame = new Registration();
+        frame.nameTextField.setText("John Doe");
+        frame.mobileTextField.setText("4a4");
+        frame.passwordField.setText("password123");
+        frame.termsCheckBox.setSelected(true);
+        frame.submitButton.doClick();
+
+        assertEquals("Please enter a valid 11-digit mobile number.", frame.resultTextArea.getText());
     }
 
     @Test
     public void testEmptyPasswordField() {
-        MyFrame frame = new MyFrame();
-        frame.setVisible(true);
-        frame.tname.setText("abc");
-        frame.tpassword.setText("");
-        frame.term.setSelected(true);
-        frame.sub.doClick();
-        assertEquals("Password cannot be empty", frame.res.getText()); // No error message unless clicked again
+        Registration frame = new Registration();
+        frame.nameTextField.setText("John Doe");
+        frame.passwordField.setText("");
+        frame.mobileTextField.setText("12345678922");
+        frame.termsCheckBox.setSelected(true);
+        frame.submitButton.doClick();
+
+        assertEquals(
+                "Please enter a strong password (at least 8 characters with uppercase, lowercase, and a special character).",
+                frame.resultTextArea.getText());
+    }
+
+    @Test
+    public void testEmptyAddress() {
+        Registration frame = new Registration();
+        frame.nameTextField.setText("John Doe");
+        frame.mobileTextField.setText("12345678390");
+        frame.passwordField.setText("passwD@ord123");
+        frame.termsCheckBox.setSelected(true);
+        frame.submitButton.doClick();
+
+        assertEquals("Please enter your address.", frame.resultTextArea.getText());
     }
 
     @Test
     public void testTermsNotAccepted() {
-        MyFrame frame = new MyFrame();
-        frame.setVisible(true);
-        frame.sub.doClick();
-        assertEquals("Please accept the terms & conditions..", frame.res.getText());
+        Registration frame = new Registration();
+        frame.nameTextField.setText("John Doe");
+        frame.passwordField.setText("@45aDssd5");
+        frame.addressTextArea.setText("123 Main Street");
+
+        frame.mobileTextField.setText("12345678922");
+        frame.submitButton.doClick();
+
+        assertEquals("Please accept the terms and conditions.", frame.resultTextArea.getText());
     }
 
     @Test
     public void testSuccessfulRegistration() {
-        MyFrame frame = new MyFrame();
-        frame.setVisible(true);
-        frame.tname.setText("John Doe");
-        frame.tmno.setText("1234567890");
-        frame.male.setSelected(true);
-        frame.date.setSelectedIndex(1);
-        frame.month.setSelectedIndex(5);
-        frame.year.setSelectedIndex(10);
-        frame.tadd.setText("123 Main Street");
-        frame.tpassword.setText("password123");
-        frame.term.setSelected(true);
-        frame.sub.doClick();
+        Registration frame = new Registration();
+        frame.nameTextField.setText("John Doe");
+        frame.mobileTextField.setText("12345678890");
+        frame.maleRadioButton.setSelected(true);
+        frame.dateComboBox.setSelectedIndex(1);
+        frame.monthComboBox.setSelectedIndex(5);
+        frame.yearComboBox.setSelectedIndex(10);
+        frame.addressTextArea.setText("123 Main Street");
+        frame.passwordField.setText("pass@5Sword123");
+        frame.termsCheckBox.setSelected(true);
+        frame.submitButton.doClick();
 
-        assertEquals("Registration Successfully..", frame.res.getText());
-        assertTrue(frame.tout.getText().contains("John Doe"));
-        assertEquals(frame.getUserFile().exists(), true);
+        assertTrue(frame.resultTextArea.getText().contains("Registration Successfully.."));
     }
 
     @Test
     public void testResetForm() {
-        MyFrame frame = new MyFrame();
-        frame.setVisible(true);
-        frame.tname.setText("John Doe");
-        frame.tmno.setText("1234567890");
-        frame.reset.doClick();
+        Registration frame = new Registration();
+        frame.nameTextField.setText("John Doe");
+        frame.mobileTextField.setText("1234567890");
+        frame.resetButton.doClick();
 
-        assertEquals("", frame.tname.getText());
-        assertEquals("", frame.tmno.getText());
-        assertEquals("", frame.tout.getText());
-        assertEquals("", frame.res.getText());
+        assertEquals("", frame.nameTextField.getText());
+        assertEquals("", frame.mobileTextField.getText());
+        assertEquals("", frame.resultTextArea.getText());
     }
-
 }
