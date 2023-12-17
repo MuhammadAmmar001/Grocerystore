fe36c112a6923558c9dcb19062d2de956d728468
MuhammadAmmar001 <muhammadammar001002@gmail.com>
2023-12-17 19:46:16 +0500

    Registration adjusted with testcase

--- Registration.java
+++ Registration.java
@@ -7,22 +7,20 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Calendar;
 
-
 class Registration extends JFrame implements ActionListener {
-    private JTextField nameTextField, mobileTextField;
-    private JPasswordField passwordField;
-    private JTextArea addressTextArea, resultTextArea;
-    private JRadioButton maleRadioButton, femaleRadioButton;
-    private JComboBox<String> dateComboBox, monthComboBox, yearComboBox;
-    private JCheckBox termsCheckBox;
-    private JButton submitButton, resetButton, BackToLogin;
+    public JTextField nameTextField, mobileTextField;
+    public JPasswordField passwordField;
+    public JTextArea addressTextArea, resultTextArea;
+    public JRadioButton maleRadioButton, femaleRadioButton;
+    public JComboBox<String> dateComboBox, monthComboBox, yearComboBox;
+    public JCheckBox termsCheckBox;
+    public JButton submitButton, resetButton, BackToLogin;
 
     public Registration() {
         setVisible(true);
         setExtendedState(JFrame.MAXIMIZED_BOTH);
         initializeUI();
-    
-        
+
     }
 
     private void initializeUI() {
@@ -34,8 +32,8 @@ class Registration extends JFrame implements ActionListener {
         nameTextField = new JTextField(20);
         mobileTextField = new JTextField(20);
         passwordField = new JPasswordField(20);
-        addressTextArea = new JTextArea(5,20);
-        resultTextArea = new JTextArea(5,20);
+        addressTextArea = new JTextArea(5, 20);
+        resultTextArea = new JTextArea(5, 20);
         maleRadioButton = new JRadioButton("Male");
         femaleRadioButton = new JRadioButton("Female");
         ButtonGroup genderGroup = new ButtonGroup();
@@ -66,7 +64,7 @@ class Registration extends JFrame implements ActionListener {
         BackToLogin.addActionListener(this);
 
         // Form Layout
-        
+
         JPanel formPanel = new JPanel(new GridBagLayout());
         GridBagConstraints constraints = new GridBagConstraints();
         constraints.anchor = GridBagConstraints.WEST;
@@ -139,37 +137,30 @@ class Registration extends JFrame implements ActionListener {
         container.add(formPanel, BorderLayout.CENTER);
         container.add(new JScrollPane(resultTextArea), BorderLayout.SOUTH);
 
-
         // Add the background image panel to the container
         container.add(new JScrollPane(resultTextArea), BorderLayout.SOUTH);
 
-        
         // Set window properties
         setTitle("Registration Form");
         setExtendedState(JFrame.MAXIMIZED_BOTH);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
-
-
     @Override
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == submitButton) {
             submit();
-            
 
         } else if (e.getSource() == resetButton) {
-         
+
             reset();
-        }
-        else if (e.getSource() == BackToLogin) {
+        } else if (e.getSource() == BackToLogin) {
             LoginFrame loginFrame = new LoginFrame();
         }
 
     }
 
-    private void reset()
-    {
+    private void reset() {
         nameTextField.setText("");
         mobileTextField.setText("");
         passwordField.setText("");
@@ -184,8 +175,7 @@ class Registration extends JFrame implements ActionListener {
         resultTextArea.setText("");
     }
 
-    private void submit()
-    {
+    private void submit() {
         // Clear Result Area
         resultTextArea.setText("");
 
@@ -194,16 +184,16 @@ class Registration extends JFrame implements ActionListener {
 
         String mobile = mobileTextField.getText();
         if (!isValidMobileNumber(mobile)) {
-            resultTextArea.append("Please enter a valid 11-digit mobile number.\n");
+            resultTextArea.append("Please enter a valid 11-digit mobile number.");
             return;
         }
 
         String password = new String(passwordField.getPassword());
         if (!isValidPassword(password)) {
-            resultTextArea.append("Please enter a strong password (at least 8 characters with uppercase, lowercase, and a special character).\n");
+            resultTextArea.append(
+                    "Please enter a strong password (at least 8 characters with uppercase, lowercase, and a special character).");
             return;
         }
-
 
         String address = addressTextArea.getText();
         String date = (String) dateComboBox.getSelectedItem();
@@ -214,8 +204,40 @@ class Registration extends JFrame implements ActionListener {
         boolean termsAccepted = termsCheckBox.isSelected();
 
         // Validate Data
-        if (name.isEmpty() || mobile.isEmpty() || password.isEmpty() || address.isEmpty() || !termsAccepted) {
-            resultTextArea.append("Please fill in all the fields correctly and accept the terms and conditions.\n");
+
+        if (name.isEmpty()) {
+            resultTextArea.append("Please enter your name.");
+            return;
+        }
+
+        else if (mobile.isEmpty()) {
+            resultTextArea.append("Please enter your mobile number.");
+            return;
+        }
+
+        else if (!isValidMobileNumber(mobile)) {
+            resultTextArea.append("Please enter a valid 11-digit mobile number.");
+            return;
+        }
+
+        else if (!isValidPassword(password)) {
+            resultTextArea.append(
+                    "Please enter a strong password (at least 8 characters with uppercase, lowercase, and a special character).");
+            return;
+        }
+
+        else if (address.isEmpty()) {
+            resultTextArea.append("Please enter your address.");
+            return;
+        }
+
+        else if (date.isEmpty() || month.isEmpty() || year.isEmpty()) {
+            resultTextArea.append("Please enter your complete date of birth.");
+            return;
+        }
+
+        else if (!termsAccepted) {
+            resultTextArea.append("Please accept the terms and conditions.");
             return;
         }
 
@@ -232,10 +254,11 @@ class Registration extends JFrame implements ActionListener {
     private boolean isValidMobileNumber(String mobile) {
         return mobile.matches("\\d{11}");
     }
-    
+
     private boolean isValidPassword(String password) {
-   
-        return password.length() >= 8 && password.matches(".*[A-Z].*") && password.matches(".*[a-z].*") && password.matches(".*[^a-zA-Z0-9].*");
+
+        return password.length() >= 8 && password.matches(".*[A-Z].*") && password.matches(".*[a-z].*")
+                && password.matches(".*[^a-zA-Z0-9].*");
     }
 
     private void processRegistration() {
@@ -263,7 +286,7 @@ class Registration extends JFrame implements ActionListener {
             resultTextArea.setText(data + "\n" + data4 + "\n" + data1 + "\n" + data2 + "\n" + data3);
             resultTextArea.setEditable(false);
             // Assuming that 'res' is a label for displaying the result
-             resultTextArea.setText("Registration Successfully..");
+            resultTextArea.setText("Registration Successfully..");
 
             // Adjust the file path based on your project structure
             File userFile = new File("userfile.txt");
@@ -278,5 +301,5 @@ class Registration extends JFrame implements ActionListener {
             }
         }
     }
-    
+
 }
